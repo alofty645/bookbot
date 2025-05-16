e899f83e0b3085567f6c5b3eadc60eed33fdb00a
alofty645 <aden.lofthouse@gmail.com>
2025-05-16 17:58:51 +1000

    finished

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from stats import *
-
-filepath = 'books/frankenstein.txt'  
+import sys
 
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
 
-
-
+def print_report(filepath, charactercount, sortedlist):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {charactercount} total words")
+    print("--------- Character Count -------")
+    for item in sortedlist:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     wordcount = number_of_words(book_text)
     charactercount = count_characters(book_text)
-    
-    print(f"{wordcount} words found in the document")
-    print(charactercount)
+    sortedlist = chars_dict_to_sorted_list(charactercount)
+    print_report(filepath, wordcount, sortedlist)
+
 main()
--- stats.py
+++ stats.py
@@ -9,5 +9,14 @@ def count_characters(book_text):
         frequency[char] = frequency.get(char, 0) + 1
     return frequency
 
-def sorting(frequency):
-    
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
